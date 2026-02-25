82324f5e473e2506be43898593cc53b8bbfcc93c
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-25 22:22:59 +0530

    chore: add relayer index.js stub

---
+++ relayer/index.js
@@ -0,0 +1,8 @@
+const { ethers } = require('ethers');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+console.log("Starting Relayer Service...");
+
+// Temporary stub index.js
+// Final implementation will connect to providers, initialize DB, and listen to events.
