deb2019ded65aeede95a3ae41238da23aba3c337
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-25 22:23:59 +0530

    feat: add deploy script stub

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+const hre = require("hardhat");
+
+async function main() {
+    console.log("Deploying contracts to local chains...");
+    // TODO: Write deployment scripts
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
