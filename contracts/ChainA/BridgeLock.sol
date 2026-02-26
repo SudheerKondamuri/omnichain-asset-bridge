ec8089568c4afc70a7f6592b6ff21de0a23cf0df
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-26 16:00:25 +0530

    refactor: update BridgeLock with SafeERC20, multi-chain support, and improved nonce tracking

--- contracts/ChainA/BridgeLock.sol
+++ contracts/ChainA/BridgeLock.sol
@@ -3,51 +3,77 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract BridgeLock is Pausable, AccessControl {
+    using SafeERC20 for IERC20;
+
     bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     IERC20 public immutable token;
-    uint256 public nextNonce; 
+    uint256 public nextNonce;
 
-    mapping(uint256 => bool) public processedNonces;
+    uint256 public immutable currentChainId;
 
-    event Locked(address indexed user, uint256 amount, uint256 nonce);
-    event Unlocked(address indexed user, uint256 amount, uint256 nonce);
+    mapping(uint256 => mapping(uint256 => bool)) public processedSourceNonces;
 
-    constructor(address _token,address _admin, address _relayer, address _pauser) {
+    event Locked(
+        address indexed user,
+        uint256 amount,
+        uint256 nonce,
+        uint256 indexed destinationChainId
+    );
+
+    event Unlocked(
+        address indexed user,
+        uint256 amount,
+        uint256 nonce,
+        uint256 indexed sourceChainId
+    );
+
+    constructor(address _token, address _relayer, uint256 _chainId) {
         token = IERC20(_token);
-        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        currentChainId = _chainId;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(RELAYER_ROLE, _relayer);
-        _grantRole(PAUSER_ROLE, _pauser);
     }
 
-    function lock(uint256 amount) external whenNotPaused {
+    function lock(
+        uint256 amount,
+        uint256 destinationChainId
+    ) external whenNotPaused {
         require(amount > 0, "Amount must be > 0");
+        require(
+            destinationChainId != currentChainId,
+            "Cannot bridge to same chain"
+        );
+
         uint256 currentNonce = nextNonce++;
-        
-        // Requirement: User must have called token.approve() first
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-        
-        emit Locked(msg.sender, amount, currentNonce);
+
+        token.safeTransferFrom(msg.sender, address(this), amount);
+
+        emit Locked(msg.sender, amount, currentNonce, destinationChainId);
+    }
+ 
+    function unlock(
+        address user,
+        uint256 amount,
+        uint256 nonce,
+        uint256 sourceChainId
+    ) external onlyRole(RELAYER_ROLE) whenNotPaused {
+        require(sourceChainId != currentChainId, "Invalid source chain");
+        require(
+            !processedSourceNonces[sourceChainId][nonce],
+            "Nonce already processed"
+        );
+
+        processedSourceNonces[sourceChainId][nonce] = true;
+
+        token.safeTransfer(user, amount);
+        emit Unlocked(user, amount, nonce, sourceChainId);
     }
 
-    function unlock(address user, uint256 amount, uint256 nonceFromChainB) 
-        external 
-        onlyRole(RELAYER_ROLE) 
-        whenNotPaused 
-    {
-        require(!processedNonces[nonceFromChainB], "Nonce already processed");
-        
-        processedNonces[nonceFromChainB] = true;
-        
-        require(token.transfer(user, amount), "Transfer failed");
-        emit Unlocked(user, amount, nonceFromChainB);
-    }
-
-    // Required for the Governance Recovery requirement
     function pause() external onlyRole(PAUSER_ROLE) {
         _pause();
     }
